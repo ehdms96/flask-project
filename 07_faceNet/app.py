2afb403aa6a86872e6726b673749a302646c39c1
ehdms96 <doeun96@naver.com>
2021-03-22 20:13:46 +0900

    divide facenet.py

--- 07_faceNet/app.py
+++ 07_faceNet/app.py
@@ -1,35 +1,26 @@
-from flask import Flask, render_template, request, send_file
-from werkzeug.utils import secure_filename
-import numpy as np
-from mtcnn import MTCNN as mt
-import cv2
-from PIL import Image
-from facenet_pytorch import MTCNN, InceptionResnetV1
-import torch
-from torch.utils.data import DataLoader
-from torchvision import datasets
-import pandas as pd
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 import os
-workers = 0 if os.name == 'nt' else 4
-
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print('Running on device: {}'.format(device))
-
-resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
-
+from facenet import main
 app = Flask(__name__)
 #app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #파일 업로드 용량 제한 단위:바이트
 
-@app.errorhandler(404)
-def page_not_found(error):
-	app.logger.error(error)
-	return render_template('page_not_found.html'), 404
+@app.route('/test')
+def test():
+        return main()
 
 #HTML 렌더링
 @app.route('/')
 def home_page():
 	return render_template('home.html')
+
+# 파일 리스트
+@app.route('/list')
+def list_page():
+	file_list = os.listdir("./uploads")
+	html = """<center><a href="/">홈페이지</a><br><br>""" 
+	html += "file_list: {}".format(file_list) + "</center>"
+	return html
 
 #업로드 HTML 렌더링
 @app.route('/upload')
@@ -44,81 +35,7 @@ def upload_file():
 		#저장할 경로 + 파일명
 		f.save('./uploads/' + secure_filename(f.filename))
 		return render_template('check.html')
-	else:
-		return render_template('page_not_found.html')
 
-#다운로드 HTML 렌더링
-@app.route('/downfile')
-def down_page():
-	files = os.listdir("./uploads")
-	return render_template('filedown.html',files=files)
-
-#파일 다운로드 처리
-@app.route('/fileDown', methods = ['GET', 'POST'])
-def down_file():
-	if request.method == 'POST':
-		sw=0
-		files = os.listdir("./uploads")
-		for x in files:
-			if(x==request.form['file']):
-				sw=1
-				path = "./uploads/" 
-				return send_file(path + request.form['file'],
-						attachment_filename = request.form['file'],
-						as_attachment=True)
-
-		return render_template('page_not_found.html')
-	else:
-		return render_template('page_not_found.html')
-
-@app.route('/faceNet')
-def main():
-    faces_dirpath = './uploads/'
-    faces_list = os.listdir(faces_dirpath)
-
-    pixels1 = extract_face(faces_dirpath + faces_list[0])
-    pixels2 = extract_face(faces_dirpath + faces_list[1])
-    mtcnn = MTCNN()
-    img1 = Image.open(faces_dirpath + faces_list[0])
-    img2 = Image.open(faces_dirpath + faces_list[1])
-    img_cropped1 = mtcnn(img1)
-    img_cropped2 = mtcnn(img2)
-    
-    img_embedding1 = resnet(torch.as_tensor(np.asarray(img_cropped1)).unsqueeze(0))
-    img_embedding2 = resnet(torch.as_tensor(np.asarray(img_cropped2)).unsqueeze(0))
-
-    dists = (img_embedding1 - img_embedding2).norm().item()
-    return 'similarity is ' + dists
-
-def extract_face(filename, required_size=(160, 160)):
-    # 파일에서 이미지 불러오기
-    image = Image.open(filename)
-    # RGB로 변환, 필요시
-    image = image.convert('RGB')
-    # 배열로 변환
-    pixels = np.asarray(image)
-    # 감지기 생성, 기본 가중치 이용
-    detector = mt()
-    # 이미지에서 얼굴 감지
-    results = detector.detect_faces(pixels)
-    # 첫 번째 얼굴에서 경계 상자 추출
-    x1, y1, width1, height1 = results[0]['box']
-    # 버그 수정
-    x1, y1 = abs(x1), abs(y1)
-    x3, y3 = x1 + width1, y1 + height1
-    # 얼굴 추출
-    face1 = pixels[y1:y3, x1:x3]
-    # 모델 사이즈로 픽셀 재조정
-    image1 = Image.fromarray(face1)
-    image1 = image1.resize(required_size)
-    filedir = []
-    for i in filename.split('/'):
-      filedir.append(i)
-    image1.save('./uploads/'+filedir[-1])
-    face_array1 = np.asarray(image1)
-    
-    return face_array1
-
+#서버 실행
 if __name__ == '__main__':
-	#서버 실행
-	app.run(host='0.0.0.0', debug = True)
+	app.run(host='0.0.0.0', port=8080, debug = True)
---
+++ 07_faceNet/facenet.py
@@ -0,0 +1,56 @@
+
+import numpy as np
+from mtcnn import MTCNN as mt
+from PIL import Image
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import os
+
+workers = 0 if os.name == 'nt' else 4
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print('Running on device: {}'.format(device))
+
+resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+
+def main():
+    required_size=(160, 160)
+    faces_dirpath = '/root/uploads/'
+    faces_list = os.listdir(faces_dirpath)
+    image1 = Image.open(faces_dirpath + faces_list[0])
+    image2 = Image.open(faces_dirpath + faces_list[1])
+    image1 = image1.convert('RGB')
+    image2 = image2.convert('RGB')
+    pixels1 = np.asarray(image1)
+    pixels2 = np.asarray(image2)
+    detector = mt()
+    results1 = detector.detect_faces(pixels1)
+    results2 = detector.detect_faces(pixels2)
+    x1, y1, width1, height1 = results1[0]['box']
+    x2, y2, width2, height2 = results2[0]['box']
+    x1, y1 = abs(x1), abs(y1)
+    x2, y2 = abs(x2), abs(y2)
+    x3, y3 = x1 + width1, y1 + height1
+    x4, y4 = x2 + width2, y2 + height2
+    face1 = pixels1[y1:y3, x1:x3]
+    face2 = pixels2[y2:y4, x2:x4]
+    image1 = Image.fromarray(face1)
+    image2 = Image.fromarray(face2)
+    image1 = image1.resize(required_size)
+    image2 = image2.resize(required_size)
+    image1.save(faces_dirpath + faces_list[0])
+    image2.save(faces_dirpath + faces_list[1])
+
+    pxl1 = np.asarray(image1)
+    pxl2 = np.asarray(image2)
+    mtcnn = MTCNN()
+    img1 = Image.open(faces_dirpath + faces_list[0])
+    img2 = Image.open(faces_dirpath + faces_list[1])
+    img_cropped1 = mtcnn(img1)
+    img_cropped2 = mtcnn(img2)
+    img_embedding1 = resnet(torch.as_tensor(np.asarray(img_cropped1)).unsqueeze(0))
+    img_embedding2 = resnet(torch.as_tensor(np.asarray(img_cropped2)).unsqueeze(0))
+    dists = (img_embedding1 - img_embedding2).norm().item()
+    return dists
